58ecdf964d3d134cde92a9cc4d1500ebb0889b5b
Gábor Kerekes <krks.gbr@gmail.com>
2025-04-02 06:38:23 +0200

    refactor(project-settings): simplify project settings builder

--- src/lib/project-settings/ProjectSettings.ts
+++ src/lib/project-settings/ProjectSettings.ts
@@ -1,131 +1,12 @@
-import { AutoEntities } from './AutoEntities'
-import { AutoKeywords } from './AutoKeywords'
-import { AutoSummary } from './AutoSummary'
-import { AutoTopics } from './AutoTopics'
-import { AutoVision } from './AutoVision'
-import { CustomSchema } from './CustomSchema'
-import { Enhancement } from './Enhancement'
-import { Generative } from './Generative'
-import { ImageVectorizer } from './ImageVectorizer'
-import { PdfParser } from './PdfParser'
-import { QnA } from './QnA'
-import { Reranker } from './Reranker'
-import { Spellcheck } from './Spellcheck'
-import { TextVectorizer } from './TextVectorizer'
-import { CoreTypes, UnbodyProjectSettingsDoc } from '../core-types'
-type Module =
-  | typeof TextVectorizer
-  | typeof ImageVectorizer
-  | typeof QnA
-  | typeof Generative
-  | typeof Reranker
-  | typeof Spellcheck
-  | typeof PdfParser
-  | typeof AutoSummary
-  | typeof AutoEntities
-  | typeof AutoKeywords
-  | typeof AutoTopics
-  | typeof AutoVision
-  | typeof CustomSchema
-  | typeof Enhancement
+import { UnbodyProjectSettingsDoc } from '../core-types'
+import {
+  TextVectorizer,
+  ImageVectorizer,
+  Generative,
+  Enhancer,
+} from '../plugins/builtin'
 
-const moduleKeys = [
-  'textVectorizer',
-  'imageVectorizer',
-  'qnaProvider',
-  'generative',
-  'reranker',
-  'spellcheck',
-  'pdfParser',
-  'autoSummary',
-  'autoEntities',
-  'autoKeywords',
-  'autoTopics',
-  'autoVision',
-  'enhancement',
-  'customSchema',
-]
-
-const module2Key = (module: any) => {
-  switch (module) {
-    case TextVectorizer:
-      return 'textVectorizer'
-    case ImageVectorizer:
-      return 'imageVectorizer'
-    case QnA:
-      return 'qnaProvider'
-    case Generative:
-      return 'generative'
-    case Reranker:
-      return 'reranker'
-    case Spellcheck:
-      return 'spellcheck'
-
-    case PdfParser:
-      return 'pdfParser'
-
-    case AutoSummary:
-      return 'autoSummary'
-    case AutoEntities:
-      return 'autoEntities'
-    case AutoKeywords:
-      return 'autoKeywords'
-    case AutoTopics:
-      return 'autoTopics'
-    case AutoVision:
-      return 'autoVision'
-
-    case CustomSchema:
-      return 'customSchema'
-    case Enhancement:
-      return 'enhancement'
-
-    default:
-      throw new Error('Invalid module')
-  }
-}
-
-const key2Module = (key: string) => {
-  if (!moduleKeys.includes(key)) {
-    throw new Error('Invalid module key')
-  }
-
-  switch (key) {
-    case 'textVectorizer':
-      return TextVectorizer
-    case 'imageVectorizer':
-      return ImageVectorizer
-    case 'qnaProvider':
-      return QnA
-    case 'generative':
-      return Generative
-    case 'reranker':
-      return Reranker
-    case 'spellcheck':
-      return Spellcheck
-
-    case 'pdfParser':
-      return PdfParser
-
-    case 'autoSummary':
-      return AutoSummary
-    case 'autoEntities':
-      return AutoEntities
-    case 'autoKeywords':
-      return AutoKeywords
-    case 'autoTopics':
-      return AutoTopics
-    case 'autoVision':
-      return AutoVision
-
-    case 'customSchema':
-      return CustomSchema
-    case 'enhancement':
-      return Enhancement
-    default:
-      throw new Error('Invalid module')
-  }
-}
+export { TextVectorizer, ImageVectorizer, Generative, Enhancer }
 
 const defaultFileParsers = {
   'image/.*': [{ name: 'file-parser-image' }],
@@ -133,69 +14,82 @@ const defaultFileParsers = {
   'application/vnd.google-apps.document': [{ name: 'file-parser-google-doc' }],
 }
 
+// Under the hood, AutoSummary and AutoVision are just enhancer plugin presets
+export namespace AutoSummary {
+  export type Model = Enhancer.Summarizer.Model
+  export const OpenAI = Enhancer.Summarizer.OpenAI
+}
+
+export namespace AutoVision {
+  export type Model = Enhancer.StructuredOutputGenerator.Model
+  export const OpenAI = Enhancer.StructuredOutputGenerator.OpenAI
+}
+
 export class ProjectSettings {
-  private textVectorizer: TextVectorizer = new TextVectorizer(
-    TextVectorizer.OpenAI.Ada002,
-  )
-  private imageVectorizer: ImageVectorizer | null = null
-  private generative: Generative = new Generative(Generative.OpenAI.GPT4o)
-  private reranker: Reranker | null = null
-    // = new Reranker(Reranker.Cohere.MultilingualV3)
+  private settingsDoc: UnbodyProjectSettingsDoc = {
+    fileParsers: defaultFileParsers,
+    textVectorizer: { name: TextVectorizer.OpenAI.embedding3Large },
+    imageVectorizer: undefined,
+    generative: undefined,
 
-  private autoSummary: AutoSummary | null = null
-  private autoVision: AutoVision | null = null
+    // built-in enhancers
+    autoSummary: undefined,
+    autoVision: undefined,
 
-  private customSchema: CustomSchema = new CustomSchema()
-  private enhancement: Enhancement = new Enhancement()
-  private fileParsers: Record<
-    string,
-    CoreTypes.ProjectSettings.ModuleConfig[]
-  > = defaultFileParsers
+    // custom enhancer pipelines (not supported at the moment)
+    enhancement: { pipelines: [] },
 
-  constructor() {
-    this.set(new TextVectorizer(TextVectorizer.Contextionary.Default))
+    // custom schema (not supported at the moment)
+    customSchema: { collections: [] },
   }
 
-  get<T extends Module = Module>(module: T) {
-    const key = module2Key(module)
-    return (this as any)[key] as InstanceType<T>
+  withTextVectorizer(
+    pluginAlias: TextVectorizer.Alias = TextVectorizer.OpenAI.embedding3Large,
+  ) {
+    this.settingsDoc.textVectorizer = { name: pluginAlias }
+    return this
   }
 
-  set(value: InstanceType<Module>) {
-    const key = module2Key(value.constructor)
-    ;(this as any)[key] = value
+  withImageVectorizer(
+    pluginAlias: ImageVectorizer.Alias = ImageVectorizer.Img2Vec.neural,
+  ) {
+    this.settingsDoc.imageVectorizer = { name: pluginAlias }
+    return this
+  }
 
+  withAutoSummary(model: AutoSummary.Model = AutoSummary.OpenAI.gpt4o) {
+    this.settingsDoc.autoSummary = {
+      name: Enhancer.summarizer,
+      options: {
+        // plugin-enhancer-summarizer expects model to be prefixed with openai-
+        model: `openai-${model}`,
+      },
+    }
+    return this
+  }
+
+  withAutoVision(model: AutoVision.Model = AutoVision.OpenAI.gpt4o) {
+    this.settingsDoc.autoVision = {
+      name: Enhancer.structuredOutputGenerator,
+      options: {
+        // plugin-enhancer-structured-output-generator expects model to be prefixed with openai-
+        model: `openai-${model}`,
+      },
+    }
+    return this
+  }
+
+  withGenerative(model: Generative.Model = Generative.OpenAI.gpt4o) {
+    this.settingsDoc.generative = {
+      name: Generative.openAI,
+      options: {
+        model: model,
+      },
+    }
     return this
   }
 
   toJSON = (): UnbodyProjectSettingsDoc => {
-    return {
-      textVectorizer: this.textVectorizer.toJSON(),
-      fileParsers: this.fileParsers,
-      enhancement: { pipelines: [] },
-      customSchema: { collections: [] },
-      imageVectorizer: this.imageVectorizer?.toJSON(),
-      reranker: this.reranker?.toJSON(),
-      generative: this.generative?.toJSON(),
-      autoSummary: this.autoSummary?.toJSON(),
-      autoVision: this.autoVision?.toJSON(),
-    }
-  }
-
-  static fromJSON = (data: any) => {
-    const settings = new ProjectSettings()
-    const keys = Object.keys(data || {}).filter((key) =>
-      moduleKeys.includes(key),
-    )
-
-    for (const key of keys) {
-      const value = data[key]
-      const module = key2Module(key)
-      if (module) {
-        settings.set((module as any).fromJSON(value))
-      }
-    }
-
-    return settings
+    return this.settingsDoc
   }
 }
