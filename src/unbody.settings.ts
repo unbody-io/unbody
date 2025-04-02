7f81f131ca164d7bb9d9c04e9c4b7253ea487eec
Gábor Kerekes <krks.gbr@gmail.com>
2025-04-02 08:06:23 +0200

    fix: make file parser settings pass validation

--- src/unbody.settings.ts
+++ src/unbody.settings.ts
@@ -2,10 +2,8 @@ import { UnbodyProjectSettingsDoc } from './lib/core-types'
 
 export const settings: UnbodyProjectSettingsDoc = {
   fileParsers: {
-    'application/vnd.google-apps.document': [
-      { name: 'file-parser-google-doc' },
-    ],
-    'image/.*': [{ name: 'file-parser-google-image' }],
+    'image/.*': [{ name: 'file-parser-image' }],
+    'text/markdown': [{ name: 'file-parser-markdown' }],
   },
   textVectorizer: {
     name: 'text2vec-openai-ada-002',
