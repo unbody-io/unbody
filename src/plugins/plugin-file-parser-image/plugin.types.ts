fc0169cf34d05c589202448958623d3a966e06e0
Hossein Mehrabi <ohyeaross@gmail.com>
2025-04-11 14:30:23 +0330

    refactor(plugin-file-parser-image): remove unused configuration types

--- src/plugins/plugin-file-parser-image/plugin.types.ts
+++ src/plugins/plugin-file-parser-image/plugin.types.ts
@@ -1,22 +1,5 @@
 import { PluginContext } from 'src/lib/plugins-common'
 
-export type Config = {
-  baseURL?: string
-
-  clientSecret: {
-    apiKey?: string
-    project?: string
-    organization?: string
-  }
-
-  options?: {
-    model?:
-      | 'text-embedding-ada-002'
-      | 'text-embedding-3-large'
-      | 'text-embedding-3-small'
-
-    autoTrim?: boolean
-  }
-}
+export type Config = {}
 
 export type Context = PluginContext
